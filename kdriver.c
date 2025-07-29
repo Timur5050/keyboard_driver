9c1284479fbb115aa37c2dfe0c87d11ae9d4a8d5
timur5050 <stukantimur811@gmail.com>
2025-07-29 09:10:48 +0000

    added checking all alt settings on the interface

--- kdriver.c
+++ kdriver.c
@@ -17,6 +17,7 @@ struct keyboard_logger {
   struct usb_device *udev;
   struct usb_interface *interface;
   unsigned char *data;
+  dma_addr_t data_dma;
 };
 
 static int keyboard_logger_probe(struct usb_interface *interface, const struct usb_device_id *id)
@@ -24,6 +25,7 @@ static int keyboard_logger_probe(struct usb_interface *interface, const struct u
   struct usb_device *udev = interface_to_usbdev(interface);
   struct usb_endpoint_descriptor *ep_desc;
   struct keyboard_logger *dev;
+  int maxp;
   
   pr_info(DRV_NAME " : probe, called for device : %s:%s\n", udev->manufacturer, udev->product);
   
@@ -32,6 +34,34 @@ static int keyboard_logger_probe(struct usb_interface *interface, const struct u
   {
     return -ENOMEM;
   }
+  
+  dev->udev = usb_get_dev(udev);
+  dev->interface = interface;
+  
+  for(int i = 0; i < dev->interface->num_altsetting; i++)
+  {
+    pr_info(DRV_NAME " : alt set num : %d\n", i);
+    struct usb_host_interface *temp = &dev->interface->altsetting[i];
+    for(int j = 0; j < temp->desc.bNumEndpoints; j++)
+    {
+      struct usb_endpoint_descriptor *temp_desc = &interface->altsetting->endpoint[i].desc;
+      pr_info(DRV_NAME ":j = %d,  type: 0x%x, address: 0x%02x, maxsize: %d\n",
+              j,
+              temp_desc->bmAttributes,
+              temp_desc->bEndpointAddress,
+              le16_to_cpu(temp_desc->wMaxPacketSize));
+      
+    }
+  }
+  
+  
+  //maxp = usb_maxpacket(udev, usb_rcvintpipe(udev, ep_desc->bEndpointAddress), usb_pipeout(usb_rcvintpipe(udev->bEndpointAddress)));
+   //dev->data = usb_alloc_coherent(udev, maxp, GFP_KERNEL, &dev->data_dma);
+  
+  //if(!dev->data)
+    //goto error;
+    
+  //dev->urb = usb_alloc_urb(0, GFP_KERNEL);
   
   usb_set_intfdata(interface, udev);
   
